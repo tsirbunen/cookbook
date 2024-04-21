3566e6087c6725d38b237aea9893522f42087a26
Satu Hyvärinen <tsirbunen@gmail.com>
2024-04-21 16:43:10 +0300

    Add some real recipes as separate files

---
+++ data/maximum-delicious-dal.js
@@ -0,0 +1,49 @@
+export const lemonyLentilSoup = {
+  recipeTitle: 'Maximum delicious dal',
+  tags: 'soup',
+  ovenNeeded: false,
+  language: 'English',
+  ingredientGroups: [
+    {
+      ingredients: [
+        { amount: 2, ingredient: 'onions (red or yellow)' },
+        { amount: 1.25, unit: 'tbsp', ingredient: 'jeera' },
+        { amount: 1, unit: 'tbsp', ingredient: 'coriander' },
+        { amount: 0.8, unit: 'tbsp', ingredient: 'turmeric' },
+        { amount: 0.5, unit: 'tsp', ingredient: 'cinnamon' },
+        { amount: 1, unit: 'tbsp', ingredient: 'mango curry spice mix (Santa Maria)' },
+        { amount: 0.8, unit: 'tbsp', ingredient: 'red chili flakes (Santa Maria)' },
+        { amount: 1, unit: 'L', ingredient: 'vegetable stock' },
+        { amount: 2, unit: 'tbsp', ingredient: 'fresh ginger (julienned)' },
+        { amount: 400, unit: 'g', ingredient: 'red lentils' },
+        { amount: 5, ingredient: 'fat garlic cloves' },
+        { amount: 1.5, ingredient: 'celery stalk' },
+        { amount: 500, unit: 'g', ingredient: 'butternut squash' },
+        { amount: 400, unit: 'g', ingredient: 'sweet potato' },
+        { amount: 2.5, unit: 'dL', ingredient: 'coconut cream' },
+        { ingredient: 'oil' },
+        { ingredient: 'salt' }
+      ]
+    }
+  ],
+  instructionGroups: [
+    {
+      instructions: [
+        'Rinse the lentils thoroughly in cold water. Soak in cold water until needed.',
+        'Heat (olive) oil in a large stockpot over medium-high heat.',
+        'Peel and chop the onions, then sauté in the pot until fairly soft.',
+        'Chop the celery and add to the onions.',
+        'Add jeera, coriander, turmeric, cinnamon, mango curry spice mix, and red chili flakes.',
+        'Peel and chop the garlic cloves, then add to the pot.',
+        'Peel and cut the squash and sweet potato to smallish cues and add to the pot.',
+        'Simmer.',
+        'Stir in the vegetable stock.',
+        'Add the rinsed lentils.',
+        'Add the coconut cream.',
+        'Cover and cook on low heat for about 1 hour. Stir well every now and then (the lentils tend to start sticking to the bottom). Add water if needed.',
+        'Add the very finely julienned ginger.',
+        'Season with salt.'
+      ]
+    }
+  ]
+}
